efdccd3042a31575fdccd3be4864092ee066c984
jaredg-ai <83607578+jaredg-ai@users.noreply.github.com>
2021-05-27 14:28:49 -0600

    Create alter-table.sql

---
+++ alter-table.sql
@@ -0,0 +1,15 @@
+alter table animals
+add location VARCHAR(50)
+
+---
+
+insert into animals (name, type, age, location)
+values ('larry', 'lion', 12, 'chicago')
+
+
+insert into animals (name, type, age, location)
+values ('gene', 'giraffe', 6, 'new york')
+
+insert into animals (name, type, age, location)
+values ('chester', 'cheeta', 4, 'texas')
+---
